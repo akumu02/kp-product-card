1193f1682ad4addcbf0476085f4075a1a32d4371
Katherine <katherine.provostelara@gmail.com>
2022-09-18 14:20:12 -0300

    version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductCard, ProductImage } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => {
+  test('debe mostrar el componente correctamente con la imagen personalizada', () => {
+    const wrapper = renderer.create(<ProductImage img="https://hola.jpg" />);
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  test('debe mostrar el componente con la imagen del producto', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product2}>
+        {() => <ProductImage img={product2.img} />}
+      </ProductCard>
+    );
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+});
